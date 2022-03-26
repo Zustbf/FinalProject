8d829b2d84f1745a54a8463e89280ee74f358e85
Zustbf <97849865+Zustbf@users.noreply.github.com>
2022-03-26 19:09:40 -0400

    Some Progress made on User input

--- resources/javascript/Scene2.js
+++ resources/javascript/Scene2.js
@@ -10,5 +10,40 @@ class Scene2 extends Phaser.Scene {
     this.character = this.add.image(config.width/2, config.height/2, "testCharacter");
 
     this.add.text(20, 20, "Test", {font: "25px Arial", fill: "red"});
+
+    //cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  //this resets the character's position to the top of the screen
+  resetCharPos(character) {
+    character.y = 0;
+  }
+
+  //this will pull the character downwards toward the bottom of the screen
+  testGrav(character, speed) {
+    character.y += speed;
+    if (character.y > config.height) {
+      this.resetCharPos(character);
+    }
+  }
+
+  update() {
+    /* *User Input still a Work in progress, commented out so that Phaser window Displays*
+    if (cursors.left.isDown) {
+    character.setVelocityX(-160);
+
+    } else if (cursors.right.isDown) {
+      character.setVelocityX(160);
+
+    } else {
+      character.setVelocityX(0);
+
+    }
+
+    if (cursors.up.isDown && character.body.touching.down) {
+      character.setVelocityY(-330);
+    }*/
+
+    this.testGrav(this.character, 1);
   }
 }
--- resources/javascript/game.js
+++ resources/javascript/game.js
@@ -1,8 +1,18 @@
+var gameSettings = {
+  playerSpeed: 200,
+}
+
 var config = {
   width: 600,
   height: 800,
   backgroundColor: 0x000000,
-  scene: [Scene1, Scene2]
+  scene: [Scene1, Scene2],
+  physics: {
+    default: "arcade",
+    arcade:{
+      debug: false
+    }
+  }
 }
 
 var game = new Phaser.Game(config);
