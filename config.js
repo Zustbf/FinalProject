58454996b97434379cd1c20f1569a4ce4f6567f9
Zustbf <97849865+Zustbf@users.noreply.github.com>
2022-04-18 15:57:02 -0400

    created config.js and moved config there

---
+++ config.js
@@ -0,0 +1,10 @@
+new Phaser.Game({
+  width: 950, 
+  height: 550, 
+  backgroundColor: '#3498db', 
+  scene: mainScene, 
+  physics: { default: 'arcade',
+    arcade: {debug: true} }, 
+  parent: 'game', 
+  pixelArt: true
+});
--- game.js
+++ game.js
@@ -159,13 +159,3 @@ class mainScene {
 
 
 
-new Phaser.Game({
-  width: 950, 
-  height: 550, 
-  backgroundColor: '#3498db', 
-  scene: mainScene, 
-  physics: { default: 'arcade',
-    arcade: {debug: true} }, 
-  parent: 'game', 
-  pixelArt: true
-});
